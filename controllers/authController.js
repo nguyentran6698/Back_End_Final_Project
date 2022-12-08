cb9d5284973596afe6b308b11bfd07ccb6cc80c9
nguyen tran <nguyen.tran6698@gmail.com>
2022-12-08 13:40:12 -0600

    .

--- controllers/authController.js
+++ controllers/authController.js
@@ -32,8 +32,8 @@ const register = async (req, res) => {
     role,
     verificationToken,
   });
-  // const origin = "http://localhost:5000";
-  const newOrigin = "https://main--classy-unicorn-d80a94.netlify.app";
+  const origin = "https://react-node-user-workflow-front-end.netlify.app";
+  //const newOrigin = 'https://react-node-user-workflow-front-end.netlify.app';
 
   // const tempOrigin = req.get('origin');
   // const protocol = req.protocol;
@@ -44,7 +44,7 @@ const register = async (req, res) => {
     name: user.name,
     email: user.email,
     verificationToken: user.verificationToken,
-    newOrigin,
+    origin,
   });
   // send verification token back only while testing in postman!!!
   res.status(StatusCodes.CREATED).json({
